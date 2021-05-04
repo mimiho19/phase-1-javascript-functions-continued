d96f65d4a52473ad9caff2f07bd93dd87a572115
Mimi Ho <somymimiho@gmail.com>
2021-05-03 20:17:20 -0500

    commit tree 6ec8c93a6a094a8edd9e7698ec32062b06ed176b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+function saturdayFun () {
+console.log("This Saturday, I want to roller-skate!")
+}
+function mondayWork () {
+    console.log("This Monday, I will go to the office.")
+}
